0b6b9efd00dd289af1f87513cffbe3e830856850
Emi-afktech <emiliorodriguezrast@gmail.com>
2021-10-19 19:49:34 -0300

    Combobox de año
    Se agrega combobox de año todavía sin funcionamiento para cargar las gráficas.

--- CUMple/CUMple/Pruebadeconexiom.Designer.cs
+++ CUMple/CUMple/Pruebadeconexiom.Designer.cs
@@ -35,6 +35,9 @@ namespace CUMple
             System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
             this.Graficadealumnos = new System.Windows.Forms.DataVisualization.Charting.Chart();
             this.cmbmeses = new System.Windows.Forms.ComboBox();
+            this.cmbAño = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.btncargardatos = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.Graficadealumnos)).BeginInit();
             this.SuspendLayout();
             // 
@@ -45,7 +48,7 @@ namespace CUMple
             this.Graficadealumnos.ChartAreas.Add(chartArea1);
             legend1.Name = "Legend1";
             this.Graficadealumnos.Legends.Add(legend1);
-            this.Graficadealumnos.Location = new System.Drawing.Point(-7, -1);
+            this.Graficadealumnos.Location = new System.Drawing.Point(-1, 0);
             this.Graficadealumnos.Name = "Graficadealumnos";
             this.Graficadealumnos.Palette = System.Windows.Forms.DataVisualization.Charting.ChartColorPalette.None;
             series1.ChartArea = "ChartArea1";
@@ -58,7 +61,7 @@ namespace CUMple
             series2.Name = "Faltas";
             this.Graficadealumnos.Series.Add(series1);
             this.Graficadealumnos.Series.Add(series2);
-            this.Graficadealumnos.Size = new System.Drawing.Size(670, 523);
+            this.Graficadealumnos.Size = new System.Drawing.Size(584, 523);
             this.Graficadealumnos.TabIndex = 0;
             this.Graficadealumnos.Text = "chart1";
             this.Graficadealumnos.Click += new System.EventHandler(this.Graficadealumnos_Click_1);
@@ -79,18 +82,51 @@ namespace CUMple
             "Octubre",
             "Noviembre",
             "Diciembre"});
-            this.cmbmeses.Location = new System.Drawing.Point(552, 49);
+            this.cmbmeses.Location = new System.Drawing.Point(606, 57);
             this.cmbmeses.Name = "cmbmeses";
             this.cmbmeses.Size = new System.Drawing.Size(86, 21);
             this.cmbmeses.TabIndex = 4;
             this.cmbmeses.Text = "Mes:";
             this.cmbmeses.SelectedIndexChanged += new System.EventHandler(this.comboxmeses_SelectedIndexChanged);
             // 
+            // cmbAño
+            // 
+            this.cmbAño.FormattingEnabled = true;
+            this.cmbAño.Location = new System.Drawing.Point(606, 125);
+            this.cmbAño.Name = "cmbAño";
+            this.cmbAño.Size = new System.Drawing.Size(86, 21);
+            this.cmbAño.TabIndex = 5;
+            this.cmbAño.Text = "Año:";
+            this.cmbAño.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged_1);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(0, 0);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // btncargardatos
+            // 
+            this.btncargardatos.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btncargardatos.Location = new System.Drawing.Point(606, 217);
+            this.btncargardatos.Name = "btncargardatos";
+            this.btncargardatos.Size = new System.Drawing.Size(86, 29);
+            this.btncargardatos.TabIndex = 7;
+            this.btncargardatos.Text = "Cargar";
+            this.btncargardatos.UseVisualStyleBackColor = true;
+            this.btncargardatos.Click += new System.EventHandler(this.btncargardatos_Click);
+            // 
             // Pruebadeconexiom
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(658, 518);
+            this.ClientSize = new System.Drawing.Size(758, 518);
+            this.Controls.Add(this.btncargardatos);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.cmbAño);
             this.Controls.Add(this.cmbmeses);
             this.Controls.Add(this.Graficadealumnos);
             this.Name = "Pruebadeconexiom";
@@ -105,5 +141,8 @@ namespace CUMple
 
         private System.Windows.Forms.DataVisualization.Charting.Chart Graficadealumnos;
         private System.Windows.Forms.ComboBox cmbmeses;
+        private System.Windows.Forms.ComboBox cmbAño;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btncargardatos;
     }
 }
--- CUMple/CUMple/Pruebadeconexiom.cs
+++ CUMple/CUMple/Pruebadeconexiom.cs
@@ -76,10 +76,35 @@ namespace CUMple
 
         private void Graficadealumnos_Click_1(object sender, EventArgs e)
         {
-
+            MySqlDataReader lectordedatos;
+            string comand = "Select year(fecha) from van group by year(fecha);";
+            conexionprograma.Open();
+            MySqlCommand comando = new MySqlCommand(comand, conexionprograma);
+            lectordedatos = comando.ExecuteReader();
+            while (lectordedatos.Read())
+            {
+                cmbAño.Items.Add(lectordedatos["year(fecha)"].ToString());
+            }
+            conexionprograma.Close();
         }
 
         private void comboxmeses_SelectedIndexChanged(object sender, EventArgs e)
+        {
+           
+          
+        }
+
+        private void checkBox12_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btncargardatos_Click(object sender, EventArgs e)
         {
             Graficadealumnos.Series["Asistencias"].Points.Clear();
             Graficadealumnos.Series["Faltas"].Points.Clear();
@@ -87,12 +112,6 @@ namespace CUMple
             meses = mesesindex + 1;
             creargrafica("Asistencias", "select count(*)'Asistencia',tipos,fecha from discipulos d join van v on d.cedula=v.cedula join clase c on v.idclase=c.idclase where v.idclase=1 and month(fecha)='" + meses + "' and concurre=1 group by v.idclase;", "select count(*)'Asistencia',tipos,fecha from discipulos d join van v on d.cedula=v.cedula join clase c on v.idclase=c.idclase where v.idclase=5 and month(fecha)='" + meses + "' and concurre=1 group by v.idclase;");
             creargrafica("Faltas", "select count(*)'Asistencia',tipos,fecha from discipulos d join van v on d.cedula=v.cedula join clase c on v.idclase=c.idclase where v.idclase=1 and month(fecha)='" + meses + "' and concurre=0 group by v.idclase;", "select count(*)'Asistencia',tipos,fecha from discipulos d join van v on d.cedula=v.cedula join clase c on v.idclase=c.idclase where v.idclase=5 and month(fecha)='" + meses + "' and concurre=0 group by v.idclase;");
-
-          
-        }
-
-        private void checkBox12_CheckedChanged(object sender, EventArgs e)
-        {
 
         }
     }
