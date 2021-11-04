60a9569160b467257cde84f2c21d24ac33e6b526
felifortibac <felifortibac@gmail.com>
2021-11-04 22:02:06 +0100

    userprofileadd
    Agrege el form de userprofileadd pero sin funcionalidad aún

---
+++ CUMple/CUMple/Userprofileadd.Designer.cs
@@ -0,0 +1,272 @@
+﻿
+namespace CUMple
+{
+    partial class Userprofileadd
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnagregar = new System.Windows.Forms.Button();
+            this.txbemail = new System.Windows.Forms.TextBox();
+            this.lblcelular = new System.Windows.Forms.Label();
+            this.txbcedula = new System.Windows.Forms.TextBox();
+            this.lblemail = new System.Windows.Forms.Label();
+            this.txbtxbnombrecompleto = new System.Windows.Forms.TextBox();
+            this.lblcedula = new System.Windows.Forms.Label();
+            this.lblprofesion = new System.Windows.Forms.Label();
+            this.txbfecnac = new System.Windows.Forms.TextBox();
+            this.txbprofesion = new System.Windows.Forms.TextBox();
+            this.lblfechnac = new System.Windows.Forms.Label();
+            this.lblnombrecompleto = new System.Windows.Forms.Label();
+            this.txbcelular = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.chbtaekwondo = new System.Windows.Forms.CheckBox();
+            this.chbkrav = new System.Windows.Forms.CheckBox();
+            this.btnvolveruserprofileadd = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnagregar
+            // 
+            this.btnagregar.Location = new System.Drawing.Point(428, 286);
+            this.btnagregar.Name = "btnagregar";
+            this.btnagregar.Size = new System.Drawing.Size(223, 41);
+            this.btnagregar.TabIndex = 0;
+            this.btnagregar.Text = "Agregar";
+            this.btnagregar.UseVisualStyleBackColor = true;
+            // 
+            // txbemail
+            // 
+            this.txbemail.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txbemail.Location = new System.Drawing.Point(674, 174);
+            this.txbemail.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txbemail.Name = "txbemail";
+            this.txbemail.Size = new System.Drawing.Size(148, 33);
+            this.txbemail.TabIndex = 30;
+            // 
+            // lblcelular
+            // 
+            this.lblcelular.AutoSize = true;
+            this.lblcelular.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblcelular.Location = new System.Drawing.Point(24, 177);
+            this.lblcelular.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblcelular.Name = "lblcelular";
+            this.lblcelular.Size = new System.Drawing.Size(96, 29);
+            this.lblcelular.TabIndex = 29;
+            this.lblcelular.Text = "Celular:";
+            // 
+            // txbcedula
+            // 
+            this.txbcedula.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txbcedula.Location = new System.Drawing.Point(245, 100);
+            this.txbcedula.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txbcedula.Name = "txbcedula";
+            this.txbcedula.Size = new System.Drawing.Size(148, 33);
+            this.txbcedula.TabIndex = 28;
+            // 
+            // lblemail
+            // 
+            this.lblemail.AutoSize = true;
+            this.lblemail.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblemail.Location = new System.Drawing.Point(423, 177);
+            this.lblemail.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblemail.Name = "lblemail";
+            this.lblemail.Size = new System.Drawing.Size(80, 29);
+            this.lblemail.TabIndex = 27;
+            this.lblemail.Text = "Email:";
+            // 
+            // txbtxbnombrecompleto
+            // 
+            this.txbtxbnombrecompleto.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txbtxbnombrecompleto.Location = new System.Drawing.Point(245, 37);
+            this.txbtxbnombrecompleto.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txbtxbnombrecompleto.Name = "txbtxbnombrecompleto";
+            this.txbtxbnombrecompleto.Size = new System.Drawing.Size(148, 33);
+            this.txbtxbnombrecompleto.TabIndex = 26;
+            // 
+            // lblcedula
+            // 
+            this.lblcedula.AutoSize = true;
+            this.lblcedula.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblcedula.Location = new System.Drawing.Point(24, 103);
+            this.lblcedula.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblcedula.Name = "lblcedula";
+            this.lblcedula.Size = new System.Drawing.Size(96, 29);
+            this.lblcedula.TabIndex = 25;
+            this.lblcedula.Text = "Cédula:";
+            // 
+            // lblprofesion
+            // 
+            this.lblprofesion.AutoSize = true;
+            this.lblprofesion.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblprofesion.Location = new System.Drawing.Point(423, 40);
+            this.lblprofesion.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblprofesion.Name = "lblprofesion";
+            this.lblprofesion.Size = new System.Drawing.Size(122, 29);
+            this.lblprofesion.TabIndex = 24;
+            this.lblprofesion.Text = "Profesión:";
+            // 
+            // txbfecnac
+            // 
+            this.txbfecnac.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txbfecnac.Location = new System.Drawing.Point(674, 100);
+            this.txbfecnac.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txbfecnac.Name = "txbfecnac";
+            this.txbfecnac.Size = new System.Drawing.Size(148, 33);
+            this.txbfecnac.TabIndex = 23;
+            // 
+            // txbprofesion
+            // 
+            this.txbprofesion.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txbprofesion.Location = new System.Drawing.Point(674, 37);
+            this.txbprofesion.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txbprofesion.Name = "txbprofesion";
+            this.txbprofesion.Size = new System.Drawing.Size(148, 33);
+            this.txbprofesion.TabIndex = 34;
+            // 
+            // lblfechnac
+            // 
+            this.lblfechnac.AutoSize = true;
+            this.lblfechnac.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblfechnac.Location = new System.Drawing.Point(423, 103);
+            this.lblfechnac.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblfechnac.Name = "lblfechnac";
+            this.lblfechnac.Size = new System.Drawing.Size(243, 29);
+            this.lblfechnac.TabIndex = 33;
+            this.lblfechnac.Text = "Fecha de nacimiento:";
+            // 
+            // lblnombrecompleto
+            // 
+            this.lblnombrecompleto.AutoSize = true;
+            this.lblnombrecompleto.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblnombrecompleto.Location = new System.Drawing.Point(24, 37);
+            this.lblnombrecompleto.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblnombrecompleto.Name = "lblnombrecompleto";
+            this.lblnombrecompleto.Size = new System.Drawing.Size(213, 29);
+            this.lblnombrecompleto.TabIndex = 32;
+            this.lblnombrecompleto.Text = "Nombre completo:";
+            // 
+            // txbcelular
+            // 
+            this.txbcelular.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txbcelular.Location = new System.Drawing.Point(245, 174);
+            this.txbcelular.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txbcelular.Name = "txbcelular";
+            this.txbcelular.Size = new System.Drawing.Size(148, 33);
+            this.txbcelular.TabIndex = 31;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(24, 259);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(124, 29);
+            this.label5.TabIndex = 35;
+            this.label5.Text = "Disciplina:";
+            // 
+            // chbtaekwondo
+            // 
+            this.chbtaekwondo.AutoSize = true;
+            this.chbtaekwondo.Location = new System.Drawing.Point(245, 265);
+            this.chbtaekwondo.Name = "chbtaekwondo";
+            this.chbtaekwondo.Size = new System.Drawing.Size(117, 24);
+            this.chbtaekwondo.TabIndex = 36;
+            this.chbtaekwondo.Text = "Taekwondo";
+            this.chbtaekwondo.UseVisualStyleBackColor = true;
+            // 
+            // chbkrav
+            // 
+            this.chbkrav.AutoSize = true;
+            this.chbkrav.Location = new System.Drawing.Point(245, 295);
+            this.chbkrav.Name = "chbkrav";
+            this.chbkrav.Size = new System.Drawing.Size(110, 24);
+            this.chbkrav.TabIndex = 37;
+            this.chbkrav.Text = "Krav Maga";
+            this.chbkrav.UseVisualStyleBackColor = true;
+            // 
+            // btnvolveruserprofileadd
+            // 
+            this.btnvolveruserprofileadd.BackColor = System.Drawing.Color.DarkRed;
+            this.btnvolveruserprofileadd.Font = new System.Drawing.Font("Microsoft Sans Serif", 21F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnvolveruserprofileadd.ForeColor = System.Drawing.Color.Transparent;
+            this.btnvolveruserprofileadd.Location = new System.Drawing.Point(758, 265);
+            this.btnvolveruserprofileadd.Name = "btnvolveruserprofileadd";
+            this.btnvolveruserprofileadd.Size = new System.Drawing.Size(64, 65);
+            this.btnvolveruserprofileadd.TabIndex = 38;
+            this.btnvolveruserprofileadd.Text = "⮎";
+            this.btnvolveruserprofileadd.UseVisualStyleBackColor = false;
+            // 
+            // Userprofileadd
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(864, 374);
+            this.Controls.Add(this.btnvolveruserprofileadd);
+            this.Controls.Add(this.chbkrav);
+            this.Controls.Add(this.chbtaekwondo);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txbprofesion);
+            this.Controls.Add(this.lblfechnac);
+            this.Controls.Add(this.lblnombrecompleto);
+            this.Controls.Add(this.txbcelular);
+            this.Controls.Add(this.txbemail);
+            this.Controls.Add(this.lblcelular);
+            this.Controls.Add(this.txbcedula);
+            this.Controls.Add(this.lblemail);
+            this.Controls.Add(this.txbtxbnombrecompleto);
+            this.Controls.Add(this.lblcedula);
+            this.Controls.Add(this.lblprofesion);
+            this.Controls.Add(this.txbfecnac);
+            this.Controls.Add(this.btnagregar);
+            this.Name = "Userprofileadd";
+            this.Text = "Userprofileadd";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnagregar;
+        private System.Windows.Forms.TextBox txbemail;
+        private System.Windows.Forms.Label lblcelular;
+        private System.Windows.Forms.TextBox txbcedula;
+        private System.Windows.Forms.Label lblemail;
+        private System.Windows.Forms.TextBox txbtxbnombrecompleto;
+        private System.Windows.Forms.Label lblcedula;
+        private System.Windows.Forms.Label lblprofesion;
+        private System.Windows.Forms.TextBox txbfecnac;
+        private System.Windows.Forms.TextBox txbprofesion;
+        private System.Windows.Forms.Label lblfechnac;
+        private System.Windows.Forms.Label lblnombrecompleto;
+        private System.Windows.Forms.TextBox txbcelular;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.CheckBox chbtaekwondo;
+        private System.Windows.Forms.CheckBox chbkrav;
+        private System.Windows.Forms.Button btnvolveruserprofileadd;
+    }
+}
---
+++ CUMple/CUMple/Userprofileadd.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CUMple
+{
+    public partial class Userprofileadd : Form
+    {
+        public Userprofileadd()
+        {
+            InitializeComponent();
+        }
+    }
+}
