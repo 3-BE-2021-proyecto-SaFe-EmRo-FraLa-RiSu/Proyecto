fb6c493270610665a0cc105cbdc9013e534aed79
Ferrari484 <santiferrari19@gmail.com>
2021-11-09 21:30:00 -0300

    interfaz 2.0
    mueve el toto

--- CUMple/CUMple/Iniciodesesion.Designer.cs
+++ CUMple/CUMple/Iniciodesesion.Designer.cs
@@ -41,10 +41,10 @@ namespace CUMple
             // txbconb
             // 
             this.txbconb.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.txbconb.Font = new System.Drawing.Font("Times New Roman", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txbconb.Location = new System.Drawing.Point(145, 164);
+            this.txbconb.Font = new System.Drawing.Font("Verdana", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txbconb.Location = new System.Drawing.Point(131, 164);
             this.txbconb.Name = "txbconb";
-            this.txbconb.Size = new System.Drawing.Size(229, 35);
+            this.txbconb.Size = new System.Drawing.Size(229, 31);
             this.txbconb.TabIndex = 1;
             this.txbconb.UseSystemPasswordChar = true;
             this.txbconb.TextChanged += new System.EventHandler(this.txbconb_TextChanged);
@@ -54,9 +54,9 @@ namespace CUMple
             // 
             this.button1.BackColor = System.Drawing.Color.Brown;
             this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
-            this.button1.Font = new System.Drawing.Font("Times New Roman", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Font = new System.Drawing.Font("Verdana", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button1.ForeColor = System.Drawing.SystemColors.Control;
-            this.button1.Location = new System.Drawing.Point(172, 205);
+            this.button1.Location = new System.Drawing.Point(131, 315);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(145, 34);
             this.button1.TabIndex = 2;
@@ -67,10 +67,10 @@ namespace CUMple
             // txbuserb
             // 
             this.txbuserb.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.txbuserb.Font = new System.Drawing.Font("Times New Roman", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txbuserb.Location = new System.Drawing.Point(145, 119);
+            this.txbuserb.Font = new System.Drawing.Font("Verdana", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txbuserb.Location = new System.Drawing.Point(131, 119);
             this.txbuserb.Name = "txbuserb";
-            this.txbuserb.Size = new System.Drawing.Size(229, 35);
+            this.txbuserb.Size = new System.Drawing.Size(229, 31);
             this.txbuserb.TabIndex = 0;
             this.txbuserb.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
             this.txbuserb.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txbuserb_KeyPress);
@@ -79,10 +79,10 @@ namespace CUMple
             // 
             this.lblemail.AutoSize = true;
             this.lblemail.BackColor = System.Drawing.Color.Transparent;
-            this.lblemail.Font = new System.Drawing.Font("Times New Roman", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblemail.Location = new System.Drawing.Point(40, 119);
+            this.lblemail.Font = new System.Drawing.Font("Verdana", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblemail.Location = new System.Drawing.Point(26, 119);
             this.lblemail.Name = "lblemail";
-            this.lblemail.Size = new System.Drawing.Size(86, 27);
+            this.lblemail.Size = new System.Drawing.Size(83, 23);
             this.lblemail.TabIndex = 3;
             this.lblemail.Text = "Usuario";
             this.lblemail.Click += new System.EventHandler(this.label1_Click);
@@ -91,10 +91,10 @@ namespace CUMple
             // 
             this.lblcontraseña.AutoSize = true;
             this.lblcontraseña.BackColor = System.Drawing.Color.Transparent;
-            this.lblcontraseña.Font = new System.Drawing.Font("Times New Roman", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblcontraseña.Location = new System.Drawing.Point(18, 164);
+            this.lblcontraseña.Font = new System.Drawing.Font("Verdana", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblcontraseña.Location = new System.Drawing.Point(4, 164);
             this.lblcontraseña.Name = "lblcontraseña";
-            this.lblcontraseña.Size = new System.Drawing.Size(121, 27);
+            this.lblcontraseña.Size = new System.Drawing.Size(117, 23);
             this.lblcontraseña.TabIndex = 4;
             this.lblcontraseña.Text = "Contraseña";
             this.lblcontraseña.Click += new System.EventHandler(this.lblcontraseña_Click);
@@ -103,10 +103,10 @@ namespace CUMple
             // 
             this.label1.AutoSize = true;
             this.label1.BackColor = System.Drawing.Color.Transparent;
-            this.label1.Font = new System.Drawing.Font("Times New Roman", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(140, 76);
+            this.label1.Font = new System.Drawing.Font("Verdana", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(136, 47);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(140, 26);
+            this.label1.Size = new System.Drawing.Size(130, 23);
             this.label1.TabIndex = 6;
             this.label1.Text = "¡Bienvenido!";
             this.label1.Click += new System.EventHandler(this.label1_Click_1);
@@ -116,9 +116,9 @@ namespace CUMple
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ActiveCaption;
-            this.BackgroundImage = global::CUMple.Properties.Resources.logo_de_Academia_DO_difuminado_;
-            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
-            this.ClientSize = new System.Drawing.Size(1084, 561);
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(484, 361);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.lblcontraseña);
             this.Controls.Add(this.lblemail);
